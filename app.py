d48fa745bfb58d0a235067befcf794b30311f21b
David Liu <davidbliu@gmail.com>
2015-09-21 17:50:15 -0700

    add golink viewing feature

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import Flask, request, jsonify, render_template
+from seeds import *
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
-
+def go_home():
+	golinks = all_golinks()
+	return render_template('golinks.html', golinks = golinks)
 if __name__ == "__main__":
     app.run()
--- seeds.py
+++ seeds.py
@@ -1,6 +1,6 @@
 from parse_rest.datatypes import Object
 from parse_rest.connection import register
-
+import sys
 application_id = 'XIpP60GkEQF4bQtKFOcceguywNhzOs3Lpsw1H17Z'
 client_key = '7otfMUAoJ7v5EjhLNMiwqKbSRUi9UOA7xnZp6zOO'
 register(application_id, client_key)
@@ -8,8 +8,14 @@ register(application_id, client_key)
 class ParseMember(Object):
 	pass
 
+class ParseGoLink(Object):
+	pass
+
+def all_golinks():
+	return ParseGoLink.Query.all().limit(sys.maxint)
 if __name__=='__main__':
 	members = ParseMember.Query.all().limit(100000)
 	member_names = [x.name for x in members]
 	print members
 	print member_names
+	print all_golinks()
