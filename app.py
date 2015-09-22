45f92107847b4d8c53ef160cf336615fad3c1ab6
David Liu <davidbliu@gmail.com>
2015-09-22 00:49:33 -0700

    add members route

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask import Flask, request, jsonify, render_template
 from seeds import *
+
 app = Flask(__name__)
 
 @app.route("/")
 def go_home():
 	golinks = all_golinks()
 	return render_template('golinks.html', golinks = golinks)
+
+@app.route('/members')
+def members_home():
+	members = [x.name for x in ParseMember.Query.all()]
+	attendance = ParseEventMember.Query.all()
+	return render_template('members.html', members = members, attendance = attendance)
+
 if __name__ == "__main__":
     app.run()
--- seeds.py
+++ seeds.py
@@ -10,9 +10,26 @@ class ParseMember(Object):
 	pass
 
 class ParseGoLink(Object):
+	def get_tags(self):
+		try:
+			return self.tags
+		except:
+			return []
+	pass
+
+class ParseGoLinkClick(Object):
 	pass
 
 class BlogPost(Object):
+	pass
+
+class ParseTablingSlot(Object):
+	pass
+
+class ParseEvent(Object):
+	pass
+
+class ParseEventMember(Object):
 	pass
 
 """ convenient access methods: examples  """
@@ -22,11 +39,20 @@ def all_blogposts():
 
 def all_golinks():
 	return ParseGoLink.Query.all().limit(sys.maxint)
+
+def speed_test():
+	return [x.get_tags() for x in all_golinks() if x.get_tags()]  # if x.key == 'fb group'] # and 'ht' in x.tags]
+
+def tag_query(tag):
+	return [x for x in all_golinks() if tag in x.get_tags()]
 if __name__=='__main__':
 	#members = ParseMember.Query.all().limit(100000)
 	#member_names = [x.name for x in members]
 	#print members
 	#print member_names
 	#print all_golinks()
-	blogposts = [x.title for x in all_blogposts()]
-	print blogposts
+	#blogposts = [x.title for x in all_blogposts()]
+	#print blogposts
+	t = tag_query('articles')
+	print [x.key for x in t]
+
