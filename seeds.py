7721ce64e62fb722d9e2834e35ee2f4a48775e9f
David Liu <davidbliu@gmail.com>
2015-09-21 18:08:58 -0700

    .

--- seeds.py
+++ seeds.py
@@ -5,17 +5,28 @@ application_id = 'XIpP60GkEQF4bQtKFOcceguywNhzOs3Lpsw1H17Z'
 client_key = '7otfMUAoJ7v5EjhLNMiwqKbSRUi9UOA7xnZp6zOO'
 register(application_id, client_key)
 
+""" Data @ WD Models """
 class ParseMember(Object):
 	pass
 
 class ParseGoLink(Object):
 	pass
 
+class BlogPost(Object):
+	pass
+
+""" convenient access methods: examples  """
+
+def all_blogposts():
+	return BlogPost.Query.all().limit(sys.maxint)
+
 def all_golinks():
 	return ParseGoLink.Query.all().limit(sys.maxint)
 if __name__=='__main__':
-	members = ParseMember.Query.all().limit(100000)
-	member_names = [x.name for x in members]
-	print members
-	print member_names
-	print all_golinks()
+	#members = ParseMember.Query.all().limit(100000)
+	#member_names = [x.name for x in members]
+	#print members
+	#print member_names
+	#print all_golinks()
+	blogposts = [x.title for x in all_blogposts()]
+	print blogposts
