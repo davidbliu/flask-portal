e11740ab2a177a16f182f64ed1a63aeea222cf6d
David Liu <davidbliu@gmail.com>
2015-09-24 20:25:07 -0700

    root url

--- ngApp/controllers.js
+++ ngApp/controllers.js
@@ -1,5 +1,5 @@
-// ROOT_URL = 'http://testing.berkeley-pbl.com';
-ROOT_URL = 'http://localhost:3000'
+ROOT_URL = 'http://testing.berkeley-pbl.com';
+// ROOT_URL = 'http://localhost:3000'
 var token = '';
 var app = angular.module('goApp', ['ngRoute']);
 app.config(function($routeProvider) {
