ce5f956b0767c8b55a94da2f58f7023c1cee2774
David Liu <davidbliu@gmail.com>
2015-11-07 01:55:59 -0800

    .

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@ sudo apt-get -y install python-pip
 sudo pip install Flask
 sudo pip install git+https://github.com/dgrtwo/ParsePy.git
 
-# elasticsearch 
-pip install Flask-Elasticsearch
+# Elasticsearch 
 # look here https://github.com/chiangf/Flask-Elasticsearch
+sudo pip install Flask-Elasticsearch
 
-# oauth
-pip install Flask-OAuth
+# Oauth
+sudo pip install Flask-OAuth
 
 
