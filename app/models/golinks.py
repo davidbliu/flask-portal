f46481369eb3e7539c0f11e800774d0356f028a6
David Liu <davidbliu@gmail.com>
2015-11-07 01:18:41 -0800

    doc

--- app/models/golinks.py
+++ app/models/golinks.py
@@ -1,9 +1,19 @@
+import json
 from ..drivers import parse_driver
 
 def test():
     r = parse_driver.make_parse_request('GET', '/1/classes/ParseMember')
     print r['results']
 
+
+def get_with_tags(tags):
+    params = {}
+    where = {}
+    where['tags'] = {'$all':tags}
+    where = json.dumps(where)
+    params['where']=where
+    results = parse_driver.make_parse_request('GET', '/1/classes/ParseGoLink', params)
+    return results['results']
 
 def recent_golinks():
     params = {}
