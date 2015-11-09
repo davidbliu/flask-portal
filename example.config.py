368b9487f8ca62379969fa09064dac1a88413652
David Liu <davidbliu@gmail.com>
2015-11-08 18:08:02 -0800

    add example config

---
+++ example.config.py
@@ -0,0 +1,11 @@
+# Parse 
+PARSE_APP_ID = '<<your parse app id here>>'
+PARSE_REST_API_KEY = '<< your parse rest api key here>>'
+
+# Google 
+GOOGLE_CLIENT_ID = '<< your google client id here>>'
+GOOGLE_CLIENT_SECRET = '<< your google client secret here>>'
+
+
+# PBL Specific
+CURRENT_SEMESTER = 'Fall 2015'
